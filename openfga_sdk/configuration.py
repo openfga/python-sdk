251684810520ddab298fca11382d6a18ab569693
Raghd Hamzeh <raghd.hamzeh@auth0.com>
2023-12-01 17:21:06 -0500

    release(v0.3.1): with dep upgrades, and reduce urllib3 min

--- openfga_sdk/configuration.py
+++ openfga_sdk/configuration.py
@@ -439,7 +439,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.1\n"\
-               "SDK Package Version: 0.3.0".\
+               "SDK Package Version: 0.3.1".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "openfga-sdk"
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 # To install the library, run the following
 #
 # python setup.py install
