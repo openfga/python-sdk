a989bddb1afbdbb376f26aba59fde74b181ba51f
Christian <cmbernard333@gmail.com>
2025-06-24 14:37:57 -0700

    Update openfga_sdk/rest.py
    Code review
    
    Co-authored-by: Evan Sims <hello@evansims.com>

--- openfga_sdk/rest.py
+++ openfga_sdk/rest.py
@@ -278,7 +278,7 @@ class RESTClientObject:
         if 200 <= response.status <= 299:
             return
 
-        if isinstance(response, aiohttp.ClientResponse):
+        if isinstance(response, aiohttp.ClientResponse) and not hasattr(response, "data"):
             response.data  = await response.read()
 
         match response.status:
