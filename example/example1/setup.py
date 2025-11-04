0604026039462957c5f26892448bcaee48307620
Raghd Hamzeh <raghd.hamzeh@auth0.com>
2025-11-04 17:22:49 -0500

    release: v0.9.8 (#244)

--- example/example1/setup.py
+++ example/example1/setup.py
@@ -15,7 +15,7 @@ from setuptools import find_packages, setup
 
 NAME = "example1"
 VERSION = "0.0.1"
-REQUIRES = ["openfga-sdk >= 0.9.7"]
+REQUIRES = ["openfga-sdk >= 0.9.8"]
 
 setup(
     name=NAME,
